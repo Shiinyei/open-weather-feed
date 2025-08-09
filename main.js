0c5a0c64014039a1c83bd6880770fdb3e1471a6b
Cyril <cyril-work@pm.me>
2025-08-09 23:26:45 +0200

    Added popup switch mechanism

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const addButton = document.getElementById('button-add');
+if(localStorage.getItem("key")){
+  browser.browserAction.setPopup({
+    popup: browser.runtime.getURL("src/opf.html"),
+  });
+}
+else{
+  browser.browserAction.setPopup({
+    popup: browser.runtime.getURL("src/welcome.html"),
+  });
+}
