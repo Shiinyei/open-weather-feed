e4ca1de8c069ae8ad15c28951ac27c987f1d1dde
Cyril <cyril-work@pm.me>
2025-08-12 23:52:39 +0200

    Improved popup change & switched from localStorage API to storage.local API

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-if(localStorage.getItem("key")){
-  browser.browserAction.setPopup({
-    popup: browser.runtime.getURL("src/opf.html"),
-  });
+function main() {
+  browser.storage.local.get().then(value => {
+    if(value.key){
+      browser.browserAction.setPopup({
+        popup: browser.runtime.getURL("src/opf.html"),
+      });
+    }
+    else{
+      browser.browserAction.setPopup({
+        popup: browser.runtime.getURL("src/welcome.html"),
+      });
+    }
+  })
 }
-else{
-  browser.browserAction.setPopup({
-    popup: browser.runtime.getURL("src/welcome.html"),
-  });
-}
+
+browser.browserAction.onClicked.addListener(() => {
+  browser.runtime.reload();
+})
+
+main();
---
+++ src/welcome.js
@@ -0,0 +1,20 @@
+const formElement = document.getElementById('welcome-form');
+
+function handleSubmit(event){
+  event.preventDefault();
+  
+  const data = new FormData(formElement);
+  const key = data.get('key');
+  
+  return browser.storage.local.set({key: btoa(key)}).then((value) => {
+    browser.browserAction.setPopup({
+      popup: browser.runtime.getURL("src/opf.html"),
+    });
+    browser.runtime.reload();
+    return;
+  });
+
+  return;
+}
+
+formElement.addEventListener('submit', (event) => handleSubmit(event))
