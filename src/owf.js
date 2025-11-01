a58a06e3717736270ef01a510b3288ca02d2319c
Cyril <github@cyrilr.fr>
2025-11-01 20:07:54 +0100

    Call to OWM API to geo-localize searched location

--- src/owf.js
+++ src/owf.js
@@ -1,6 +1,8 @@
 const formElement = document.getElementById('owf-form-new');
 const inputElement = document.getElementById('owf-input-new-location');
 
+const OWM_GEO_ENDPOINT = 'https://api.openweathermap.org/geo/1.0/direct';
+
 /**
  * [Source](https://stackoverflow.com/a/75988895)
  */
@@ -15,11 +17,20 @@ const debounce = (callback, wait) => {
 }
 
 inputElement.addEventListener('input', debounce((event) => {
-  browser.storage.local.get('key').then(
-    ({key}) => {
+  browser.storage.local.get('key')
+    .then(({key}) => {
       const searchStr = event.target.value;
       const apiKey = atob(key);
-      return apiKey;
-    }
-  )
+      return fetch(
+        `${OWM_GEO_ENDPOINT}?appid=${apiKey}&q=${searchStr}&limit=5`
+      );
+    })
+    .then((response) => {
+      return response.json();
+    })
+    .then((response) => {
+      console.log('RESPONSE',JSON.stringify(response));
+      return response;
+    })
+
 }, 500))
